fe4d2fd013a6bde185dec8486055414a0f70ed9c
juan-rod <juan.j.rodriguez2@gmail.com>
2019-04-15 18:46:06 -0500

    pushing up changes in transition-squares

---
+++ JSLesson/app.js
@@ -0,0 +1,6 @@
+
+// var div = document.createElement('div');
+// console.log('div', [div])
+// console.log('document', [document][0])
+// div.innerText = "looove JavaScript!";
+// document.body.appendChild(div);
